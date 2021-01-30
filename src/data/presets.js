d03b67dd30c60a97a630f091f82ca012b8f93e15
Maike Rees <maikefer@users.noreply.github.com>
2021-01-30 12:36:39 +0100

    Update presets.js

--- src/data/presets.js
+++ src/data/presets.js
@@ -7,38 +7,23 @@ export const PRESET1 = {
   background: '',
   removeWinning: false,
   prizes: [
-    { name: '$550', freq: 3, bg: '#CE93D8', text: '#222', hide: false, fontMod: 1 },
-    { name: '$400', freq: 3, bg: '#FFF59D', text: '#222', hide: false, fontMod: 1 },
-    { name: '$300', freq: 3, bg: '#F48FB1', text: '#222', hide: false, fontMod: 1 },
-    { name: '$900', freq: 3, bg: '#ef9a9a', text: '#222', hide: false, fontMod: 1 },
-    { name: '$500', freq: 3, bg: '#FFCC80', text: '#222', hide: false, fontMod: 1 },
-    { name: '$300', freq: 3, bg: '#80CBC4', text: '#222', hide: false, fontMod: 1 },
-    { name: '$900', freq: 3, bg: '#FFCC80', text: '#222', hide: false, fontMod: 1 },
-    { name: 'BANKRUPT', freq: 3, bg: '#333', text: '#eee', hide: false, fontMod: 1 },
-    { name: '$600', freq: 3, bg: '#CE93D8', text: '#222', hide: false, fontMod: 1 },
-    { name: '$400', freq: 3, bg: '#FFF59D', text: '#222', hide: false, fontMod: 1 },
-    { name: '$300', freq: 3, bg: '#F48FB1', text: '#222', hide: false, fontMod: 1 },
-    { name: 'LOSE A TURN', freq: 3, bg: 'white', text: '#222', hide: false, fontMod: 1 },
-    { name: '$800', freq: 3, bg: '#ef9a9a', text: '#222', hide: false, fontMod: 1 },
-    { name: '$350', freq: 3, bg: '#CE93D8', text: '#222', hide: false, fontMod: 1 },
-    { name: '$450', freq: 3, bg: '#F48FB1', text: '#222', hide: false, fontMod: 1 },
-    { name: '$700', freq: 3, bg: '#80CBC4', text: '#222', hide: false, fontMod: 1 },
-    { name: '$300', freq: 3, bg: '#FFCC80', text: '#222', hide: false, fontMod: 1 },
-    { name: '$600', freq: 3, bg: '#CE93D8', text: '#222', hide: false, fontMod: 1 },
-    { name: '$2500', freq: 3, bg: '#81D4FA', text: '#222', hide: false, fontMod: 1 },
-    { name: '$300', freq: 3, bg: '#FFF59D', text: '#222', hide: false, fontMod: 1 },
-    { name: '$600', freq: 3, bg: '#ef9a9a', text: '#222', hide: false, fontMod: 1 },
-    { name: '$300', freq: 3, bg: '#FFCC80', text: '#222', hide: false, fontMod: 1 },
-    { name: '$500', freq: 3, bg: '#80CBC4', text: '#222', hide: false, fontMod: 1 },
-    { name: 'BANKRUPT', freq: 1, bg: '#333', text: '#eee', hide: false, fontMod: 2 },
-    { name: '$10000', freq: 1, bg: '#FFD54F', text: '#222', hide: false, fontMod: 2 },
-    { name: 'BANKRUPT', freq: 1, bg: '#333', text: '#eee', hide: false, fontMod: 2 }
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#CE93D8', text: '#222', hide: false, fontMod: 1.5 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#FFF59D', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#F48FB1', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#ef9a9a', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#FFCC80', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#80CBC4', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#FFCC80', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#333', text: '#eee', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#CE93D8', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#FFF59D', text: '#222', hide: false, fontMod: 1 },
+    { name: 'Finde 1 Adjektiv', freq: 3, bg: '#F48FB1', text: '#222', hide: false, fontMod: 1 }
   ]
 };
 
 // Wheel of Misfortune: SMRT disruption edition.
 export const PRESET2 = {
-  name: 'Wheel of Misfortune',
+  name: 'Wheel of Verbes',
   winningText: 'Congratulations! You win: <b>%s</b>!',
   background: 'static/smrt.jpg',
   removeWinning: false,
@@ -63,7 +48,7 @@ export const PRESET2 = {
 
 // Wheel of Inquiry: select a person to answer your question.
 export const PRESET3 = {
-  name: 'Wheel of Inquiry',
+  name: 'Wheel of Hello',
   winningText: 'Your turn to answer the question, <b>%s</b>!',
   background: '',
   removeWinning: true,
